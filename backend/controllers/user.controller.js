c33577b048e7ad24c732d83f31731b84bed699e2
Charanjeet Singh <cjeet336@gmail.com>
2025-11-13 16:43:34 +0400

    Follow & Unfolloe APi created

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,60 @@
+import User from "../models/user.model.js";
+
+// API to get user profile
+export const getUserProfile = async () =>
+{
+    const {username} = req.params;
+
+    try {
+        const user = await User.fineOne({username}).select("-password")
+        if(!user)
+        {
+            return res.status(400).json({message:"User not found"})
+        }
+        res.status(200).json(user)
+    } catch (error) {
+        console.log("Error" , error.message)
+        res.status(500).json({error:"Internale Server Error"})
+    }
+}
+
+// API function to Follow and UnFollow
+
+export const followUnfollowUser = async (req, res) =>
+{
+    const {id} = req.params;
+    const userToModify = await User.findById(id)
+    const currentUser = await User.findById(req.user._id) 
+    if(id === req.user._id.toString()) 
+    {
+        return res.status(400).json({error:"You cannot follow/unfollow your self"})
+    }
+    if(!userToModify || !currentUser) 
+    {
+        return res.status(400).json({error:"User not found"})
+    }
+
+    const isFollowing = currentUser.following.includes(id) 
+    if(isFollowing) 
+    {
+        // unfollow the user
+        await User.findByIdAndUpdate(id, {$pull: {followers:req.user._id}})
+        await User.findByIdAndUpdate(req.user._id, {$pull: {following:id}})
+        res.status(200).json({message:"User unfollowed successfully"})
+    }
+    else
+    {
+        // Follow the user
+        await User.findByIdAndUpdate(id, {$push: {followers:req.user._id}})
+        await User.findByIdAndUpdate(req.user._id, {$push: {following:id}})
+        res.status(200).json({message:"User followed successfully"})
+        // Send Notification
+
+    }
+
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
 import { connectDB } from "./config/db.js";
 import cors from "cors"
 const app = express()
@@ -8,16 +9,18 @@ import cookieParser from "cookie-parser";
 
 dotenv.config()
 app.use(cors()) //Allow All Origin
-app.use(cookieParser()) //Allow cookies to save
+app.use(cookieParser()) //Allow to parse cookies
 const PORT = process.env.PORT || 8080
 
 app.use(express.json()) // to parse req.body
 app.use(express.urlencoded({extended:true})) // for x-www-form  in postman to parse form data in url coded
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () =>
 {
     connectDB()
     console.log("Server is running at " + `http://localhost:${PORT}`)
 })
+

6d1776d71677a03c2283314bea29a4cdee15202f
Charanjeet Singh <cjeet336@gmail.com>
2025-11-13 17:00:32 +0400

    Added Notification Model

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-
+import Notification from "../models/notification.model.js";
 // API to get user profile
 export const getUserProfile = async () =>
 {
@@ -47,8 +47,16 @@ export const followUnfollowUser = async (req, res) =>
         // Follow the user
         await User.findByIdAndUpdate(id, {$push: {followers:req.user._id}})
         await User.findByIdAndUpdate(req.user._id, {$push: {following:id}})
-        res.status(200).json({message:"User followed successfully"})
         // Send Notification
+         const newNotification = new Notification({
+            type:"follow",
+            from: req.user._id,
+            to:userToModify._id,
+         })
+          await newNotification.save()
+        //   Later change send the user id instead of message
+        res.status(200).json({message:"User followed successfully"})
+        
 
     }
 

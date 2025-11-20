0376473435c62cf411b3629708089cb2ea094b96
Charanjeet Singh <cjeet336@gmail.com>
2025-11-20 15:53:50 +0400

    Single Notification controller added

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -12,9 +12,10 @@ export const getNotifications = async (req,res) =>
         res.status(200).json(notification)
     } catch (error) {
         console.log("Error while getting notification", error.message)
-        res.status(500).json("internal server error")
+        res.status(500).json({error:"internal server error"})
     }
 }
+
 
 export const deleteNotifications = async (req,res) =>
 {
@@ -24,6 +25,28 @@ export const deleteNotifications = async (req,res) =>
         res.status(200).json({message:"Notification deleted successfully"}) 
     } catch (error) {
         console.log("Error while deleting notification", error.message)
-        res.status(500).json("internal server error")
+        res.status(500).json({error:"internal server error"})
+    }
+}
+
+export const deleteOneNotifications = async (req,res) =>
+{
+    try {
+        const notificationId = req.params.id 
+        const userId = req.user._id  
+        const notification = await Notification.findById(notificationId)
+        if(!notification)
+        {
+            return res.status(400).json({error:"Notification does not found"})
+        }
+        if(notification.to.toString() !== userId.toString())
+        {
+            return res.status(400).json({error:"You are not allowed to delete the notification"})
+        }
+        await Notification.findByIdAndDelete(notificationId)
+        res.status(200).json({message:"Notification deleted successfully"})
+    } catch (error) {
+       console.log("Error while deleting notification", error.message)
+        res.status(500).json({error:"internal server error"}) 
     }
 }

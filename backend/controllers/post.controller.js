4a1ae75339c9ab4cc36b6a56b0336e2ba82b7a26
Charanjeet Singh <cjeet336@gmail.com>
2025-11-20 14:55:42 +0400

    Formatted

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -27,9 +27,8 @@ export const getAllPosts = async (req, res) => {
 };
 
 export const getFollowingPosts = async (req, res) => {
- 
   try {
-     const userId = req.user._id;
+    const userId = req.user._id;
     const user = await User.findById(userId);
     if (!user) {
       return res.status(400).json({ error: "User not found" });
@@ -103,17 +102,16 @@ export const createPost = async (req, res) => {
   }
 };
 
-export const getUserPosts = async (req,res) =>
-{
+export const getUserPosts = async (req, res) => {
   try {
-    const {username} = req.params
-    const user = await User.findOne({username})
-    if(!username)
-    {
-      return res.status(400).json({error:"User not found"})
+    const { username } = req.params;
+    const user = await User.findOne({ username });
+    if (!username) {
+      return res.status(400).json({ error: "User not found" });
     }
-    const posts = await Post.find({user: user._id}).sort({createdAt:-1})
-    .populate({
+    const posts = await Post.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate({
         path: "user",
         select: "-password",
       })
@@ -121,12 +119,12 @@ export const getUserPosts = async (req,res) =>
         path: "comments.user",
         select: "-password",
       });
-      res.status(200).json(posts)
+    res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
     console.log("Error while fetching user posts", error.message);
   }
-}
+};
 export const likeUnlikePost = async (req, res) => {
   try {
     const userId = req.user._id;
